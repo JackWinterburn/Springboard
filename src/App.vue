2d7a66373fefa1e6437057cab79c6320a6917c54
JackWinterburn <jack.winterburn4@hotmail.co.uk>
2020-06-01 21:43:17 +0100

    implemented calendar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <v-app>
-    
-    
+    <v-content>
+      <Calendar />
+    </v-content>
   </v-app>
 </template>
 
 <script>
+import Calendar from "./components/Calendar";
 
 export default {
   name: 'App',
 
   components: {
+    Calendar
   },
 
   data: () => ({
